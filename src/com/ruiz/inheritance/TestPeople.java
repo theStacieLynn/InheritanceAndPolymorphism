ebc74601ab64630402b7bda28fb17a1ea3318068
Stacie Ruiz <s_lynn.r@outlook.com>
2022-11-30 14:47:47 -0600

    first lab complete

---
+++ src/com/ruiz/inheritance/TestPeople.java
@@ -0,0 +1,44 @@
+package com.ruiz.inheritance;
+
+public class TestPeople {
+
+	public static void main(String[] args) {
+		Person aPerson = new Person("Fred");
+		Boy jimmy = new Boy();
+		Girl betty = new Girl("Betty");
+		
+		System.out.println(aPerson);
+		System.out.println(aPerson.talk());
+		System.out.println(aPerson.walk());
+		System.out.println();
+		
+		System.out.println(jimmy);
+		System.out.println(jimmy.talk());
+		System.out.println(jimmy.walk());
+		System.out.println();
+		
+		System.out.println(betty);
+		System.out.println(betty.talk());
+		System.out.println(betty.walk());
+		System.out.println();
+		
+		System.out.println((Person)jimmy);
+	    System.out.println(((Person)jimmy).talk());
+	    System.out.println(((Person)jimmy).walk());
+	    System.out.println();
+	    
+
+	    System.out.println((Person)betty);
+	    System.out.println(((Person)betty).talk());
+	    System.out.println(((Person)betty).walk());
+	    System.out.println();
+
+	    System.out.println(Person.lifeSpan());
+	    System.out.println(Boy.lifeSpan());
+	    System.out.println(Girl.lifeSpan());
+
+	    //System.out.println(((Boy)aPerson).talk());
+
+	}
+
+}
